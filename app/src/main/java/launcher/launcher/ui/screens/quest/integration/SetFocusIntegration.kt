2ea101bc8bf32e8222472bf320953c1aeef578d1
Nethical <aguptaq88@gmail.com>
2025-02-12 09:12:29 +0530

    load apps in setup focus integeration

--- app/src/main/java/launcher/launcher/ui/screens/quest/integration/SetFocusIntegration.kt
+++ app/src/main/java/launcher/launcher/ui/screens/quest/integration/SetFocusIntegration.kt
@@ -5,14 +5,17 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
@@ -20,16 +23,20 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import kotlinx.coroutines.withContext
+import launcher.launcher.models.AppInfo
 import launcher.launcher.ui.navigation.Screen
 import launcher.launcher.ui.screens.quest.integration.components.SetFocusTimeUI
+import launcher.launcher.utils.getCachedApps
 
 
 @Composable
 fun SetFocusIntegration(
     navController: NavController
 ) {
+    val context = LocalContext.current
+    val apps = getCachedApps(context)
 
-    val apps = listOf("Chrome", "YouTube", "WhatsApp", "Spotify", "Telegram")
     var selectedApps by remember { mutableStateOf(setOf<String>()) }
 
     Scaffold(
@@ -64,38 +71,40 @@ fun SetFocusIntegration(
                 .padding(paddingValues)
                 .padding(horizontal = 16.dp, vertical = 8.dp) // Consistent padding
         ) {
-            SetFocusTimeUI()
-            Spacer(modifier = Modifier.height(32.dp))
-            Text(
-                "Select Unrestricted Apps",
-                modifier = Modifier.padding(bottom = 16.dp),
-                fontWeight = FontWeight.Bold
-            )
             LazyColumn(
                 modifier = Modifier.fillMaxSize()
             ) {
+                item {
+                    SetFocusTimeUI()
+                    Spacer(modifier = Modifier.height(32.dp))
+                    Text(
+                        "Select Unrestricted Apps",
+                        modifier = Modifier.padding(bottom = 16.dp),
+                        fontWeight = FontWeight.Bold
+                    )
+                }
                 items(apps) { app ->
                     Row(
                         modifier = Modifier
                             .fillMaxWidth()
                             .clickable {
-                                selectedApps = if (selectedApps.contains(app)) {
-                                    selectedApps - app
+                                selectedApps = if (selectedApps.contains(app.packageName)) {
+                                    selectedApps - app.packageName
                                 } else {
-                                    selectedApps + app
+                                    selectedApps + app.packageName
                                 }
                             }
                             .padding(vertical = 6.dp),
                         verticalAlignment = Alignment.CenterVertically
                     ) {
                         Checkbox(
-                            checked = selectedApps.contains(app),
+                            checked = selectedApps.contains(app.packageName),
                             onCheckedChange = { isChecked ->
-                                selectedApps = if (isChecked) selectedApps + app else selectedApps - app
+                                selectedApps = if (isChecked) selectedApps + app.packageName else selectedApps - app.packageName
                             }
                         )
                         Text(
-                            app,
+                            app.name,
                             modifier = Modifier.padding(start = 8.dp)
                         )
                     }
