eb297a1fcff015f8b33dc9e2b3ec3a7ead4d8a6a
Nethical <aguptaq88@gmail.com>
2025-02-14 16:52:18 +0530

    animate

--- app/src/main/java/launcher/launcher/ui/screens/quest/SetupNewQuest.kt
+++ app/src/main/java/launcher/launcher/ui/screens/quest/SetupNewQuest.kt
@@ -1,5 +1,13 @@
 package launcher.launcher.ui.screens.quest
 
+import androidx.activity.compose.BackHandler
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.togetherWith
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -10,6 +18,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import launcher.launcher.Constants
 import launcher.launcher.ui.navigation.QuestSetupScreen
 import launcher.launcher.ui.screens.quest.setup.SetAppFocusIntegration
@@ -21,9 +30,10 @@ import launcher.launcher.ui.screens.quest.setup.components.Navigation
 @Composable
 fun SetupNewQuest(
     quitAddNew: () -> Unit
-){
+) {
 
     val currentScreen = remember { mutableStateOf(QuestSetupScreen.Integration.route) }
+    var isNavigatingForward = remember { mutableStateOf(true) }
 
     val selectedIntegration = remember { mutableStateOf<Int?>(Constants.INTEGRATION_ID_APP_FOCUS) }
 
@@ -41,38 +51,90 @@ fun SetupNewQuest(
 
 
 
-    Scaffold(
-    floatingActionButton = {
-        Navigation(onNextPressed = {
-            currentScreen.value = nextScreenId.value
-        },
-            onBackPressed = {
-                if(isBackButtonFinish.value){
-                    quitAddNew()
-                }else{
-                    currentScreen.value = previousScreenId.value
-                }
-            },
-            isBackButtonFinish = isBackButtonFinish
-        )
-
+    BackHandler {
+        if (isBackButtonFinish.value) {
+            quitAddNew()
+        } else {
+            isNavigatingForward.value = false
+            currentScreen.value = previousScreenId.value
+        }
     }
+
+    Scaffold(
+        floatingActionButton = {
+            Navigation(
+                onNextPressed = {
+
+                    isNavigatingForward.value = true
+                    currentScreen.value = nextScreenId.value
+                },
+                onBackPressed = {
+                    if (isBackButtonFinish.value) {
+                        quitAddNew()
+                    } else {
+                        isNavigatingForward.value = false
+                        currentScreen.value = previousScreenId.value
+                    }
+                },
+                isBackButtonFinish = isBackButtonFinish
+            )
+
+        }
     )
-    {
-        paddingValues ->
-        Column(
+    { paddingValues ->
+        Box(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(paddingValues)
                 .padding(horizontal = 16.dp, vertical = 8.dp) // Consistent padding
         ) {
-            when(currentScreen.value){
-                QuestSetupScreen.Integration.route -> SetIntegration(previousScreenId,nextScreenId,isBackButtonFinish,selectedIntegration)
-                QuestSetupScreen.QuestInfo.route -> SetQuestMetaInfo(previousScreenId,nextScreenId,isBackButtonFinish,instructions,reward,questTitle,selectedIntegration)
-                QuestSetupScreen.FocusIntegration.route -> SetFocusIntegration(previousScreenId,nextScreenId,isBackButtonFinish,selectedUnrestrictedApps)
-                QuestSetupScreen.AppFocusIntegration.route -> SetAppFocusIntegration(previousScreenId,nextScreenId,isBackButtonFinish,selectedFocusApp)
+            AnimatedContent(
+                targetState = currentScreen.value,
+                transitionSpec = {
+                    if (isNavigatingForward.value) {
+                        slideInHorizontally { it } + fadeIn() togetherWith slideOutHorizontally { -it } + fadeOut()
+                    } else {
+                        slideInHorizontally { -it } + fadeIn() togetherWith slideOutHorizontally { it } + fadeOut()
+                    }
+                },
+                label = "ScreenTransition"
+            ) { targetScreen ->
+                Column {
+                    when (targetScreen) {
+                        QuestSetupScreen.Integration.route -> SetIntegration(
+                            previousScreenId,
+                            nextScreenId,
+                            isBackButtonFinish,
+                            selectedIntegration
+                        )
+
+                        QuestSetupScreen.QuestInfo.route -> SetQuestMetaInfo(
+                            previousScreenId,
+                            nextScreenId,
+                            isBackButtonFinish,
+                            instructions,
+                            reward,
+                            questTitle,
+                            selectedIntegration
+                        )
+
+                        QuestSetupScreen.FocusIntegration.route -> SetFocusIntegration(
+                            previousScreenId,
+                            nextScreenId,
+                            isBackButtonFinish,
+                            selectedUnrestrictedApps
+                        )
+
+                        QuestSetupScreen.AppFocusIntegration.route -> SetAppFocusIntegration(
+                            previousScreenId,
+                            nextScreenId,
+                            isBackButtonFinish,
+                            selectedFocusApp
+                        )
+                    }
+                }
             }
         }
-    }
 
+    }
 }
